b35b0b919c71c3f42de2dccc2141659b3c9fe037
WELCOME NAMITA <cozytuba1533@gmail.com>
2022-11-23 00:29:26 +0530

    HR-Printing_Tokens

---
+++ Hackerrank_Printing-Tokens.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int start;
+    int stop;
+    int index=0;
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    // Write your logic to print the tokens of the sentence here.
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i] == ' ')
+        {
+
+            s[i] ='\n';
+        }
+        
+    }
+    printf("%s\n",s);
+    free(s);
+    return 0;
+}
--- tempCodeRunnerFile.c
+++
@@ -1,1 +0,0 @@
-ks, number_o
