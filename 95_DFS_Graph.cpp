1bc9f3b81a35d81780da2be8bc7e7c26b36fe92a
cozytuba <cozytuba1533@gmail.com>
2024-01-13 15:41:47 +0530

    depth first search using recursion in graph

---
+++ 95_DFS_Graph.cpp
@@ -0,0 +1,49 @@
+#include<unordered_map>
+#include<list>
+#include<set>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+void dfs(int node,unordered_map<int,list<int>>&adj, unordered_map<int,bool>&v, vector<int>&comp)
+{
+    comp.push_back(node);
+    v[node]=1;
+    for(auto i:adj[node])
+    {
+        if(!v[i])
+        {
+            dfs(i,adj,v,comp);
+        }
+    }
+    
+}
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+{
+    // Write your code here
+    unordered_map<int,list<int>>adj;
+    for(int i=0;i<edges.size();i++)
+    {
+        adj[edges[i][0]].push_back(edges[i][1]);
+        adj[edges[i][1]].push_back(edges[i][0]);
+
+    }
+    vector<vector<int>>ans;
+    unordered_map<int,bool>v;
+    for(int i=0;i<V;i++)
+    {
+        vector<int>comp;
+        if(!v[i])
+        {
+            dfs(i,adj,v,comp);
+            ans.push_back(comp);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+
+}
